d63d41589dd33da0711fa9346b98c917e20a75a5
Fernanda <ferbarrosgirade@gmail.com>
2024-08-30 13:27:15 -0300

    create .env

--- core/settings.py
+++ core/settings.py
@@ -21,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY configurado pela variável de ambiente
 SECRET_KEY = config('DJANGO_SECRET_KEY', default='default_secret_key_for_development')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# Debug apenas para desenvolvimento local
+DEBUG = config('DEBUG', default='False') == 'True'
 
-ALLOWED_HOSTS = ['startupse-production.up.railway.app', 'startupse.railway.internal']
+# Permitir acesso de todos os hosts durante o desenvolvimento local
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 
 # Application definition
